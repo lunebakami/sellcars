b78e010e5e7ea9a486f8ec12b24872dde1f737d2
Luis Santos <luis.santos.taiba@gmail.com>
2022-11-23 18:35:08 -0300

    feat: routes

--- src/router.ts
+++ src/router.ts
@@ -1,3 +1,15 @@
 import { Router } from 'express';
+import authMiddleware from './app/middleware/auth';
+
+import { createSale } from './app/useCases/createSale';
+import { createUser } from './app/useCases/createUser';
+import { loginUser } from './app/useCases/loginUser';
 
 export const router = Router();
+
+router.post('/login', loginUser);
+router.post('/user', createUser);
+
+router.use(authMiddleware);
+
+router.post('/sale', createSale);
